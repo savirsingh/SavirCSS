3b0ae585b9691b5e45d756e4c04b626ef0bae7ce
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2022-07-29 18:56:26 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -24,3 +24,18 @@ function slideshowgo(n) {
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
 }
+
+var accord = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < accord.length; i++) {
+  accord[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
